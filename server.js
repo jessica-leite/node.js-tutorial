ae82d169ce3b8754de35dff750c40fd389ca9ebb
jessica.leite <jessica.leite@zappts.com.br>
2022-04-26 15:47:57 -0300

    first commit - os logs

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("Hey you");
+// console.log(global);
+
+const os = require('os');
+
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
