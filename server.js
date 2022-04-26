a6ba84029eeab2cc2709e8d73b8339428c30732d
jessica.leite <jessica.leite@zappts.com.br>
2022-04-26 17:43:41 -0300

    math log

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ console.log("Hey you");
 
 const os = require('os');
 const path = require('path');
+const math = require('./math');
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+console.log(math.add(2,3));
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.parse(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
