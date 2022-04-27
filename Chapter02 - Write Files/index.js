50e89e59b2977605ddd4924f5377a10fb3164943
jessica.leite <jessica.leite@zappts.com.br>
2022-04-27 11:08:37 -0300

    exception log

--- Chapter02 - Write Files/index.js
+++ Chapter02 - Write Files/index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
-fs.readFile('./files/starter.txt', 'utf8', (err, data) => {
+fs.readFile('./files/hello.txt', 'utf8', (err, data) => {
     if(err) throw err;
 
     console.log(data);
 });
+
+//exit on uncaught errors
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
