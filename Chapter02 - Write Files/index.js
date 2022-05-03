b983d4599d86700f98903ab6ee857d2eeee5b3f5
jessica.leite <jessica.leite@zappts.com.br>
2022-05-03 17:44:56 -0300

    unlink to remove file

--- Chapter02 - Write Files/index.js
+++ Chapter02 - Write Files/index.js
@@ -5,6 +5,7 @@ const fileOps = async () => {
     try{
         const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
         console.log(data);
+        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
         await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
         await fsPromises.appendFile(path.join(__dirname, 'files','promiseWrite.txt'), '\n\nNice to meet you.')
         await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'))
@@ -17,26 +18,6 @@ const fileOps = async () => {
 }
 
 fileOps();
-
-// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (err) => {
-//     if(err) throw err;
-
-//     console.log('Write complete');
-
-//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is.', (err) => {
-//         if(err) throw err;
-    
-//         console.log('Append complete');
-
-//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-//             if(err) throw err;
-        
-//             console.log('Rename complete');
-//         });
-//     });
-// });
-
-
 
 //exit on uncaught errors
 process.on('uncaughtException', err => {
